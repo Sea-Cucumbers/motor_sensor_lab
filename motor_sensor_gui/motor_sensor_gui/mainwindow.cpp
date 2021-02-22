35e23cb457baa4e68b09092aab077a89d485bfd1
Anoop Bhat <noopy.g.bhat@gmail.com>
2021-02-22 17:44:25 -0500

    Commands seem to be getting to the arduino, but servo control isn't working

--- motor_sensor_gui/motor_sensor_gui/mainwindow.cpp
+++ motor_sensor_gui/motor_sensor_gui/mainwindow.cpp
@@ -39,11 +39,6 @@ MainWindow::MainWindow(QWidget *parent)
     QTimer *timer = new QTimer(this);
     connect(timer, &QTimer::timeout, this, &MainWindow::timerDown);
     timer->start(100);
-
-    // All motors start in GUI mode
-    servoControlMethod = 0;
-    dcControlMethod = 0;
-    stepperControlMethod = 0;
 }
 
 void MainWindow::readyRead()
@@ -67,6 +62,7 @@ void MainWindow::timerDown()
         ui->potentiometerTextEdit->setPlainText(show);
     } else {
         qDebug() << "Unknown message type received from MCU\n";
+        qDebug() << show << endl;
     }
 
     serialBuf = serialBuf.remove(0, newlineIdx + 1);
@@ -74,19 +70,12 @@ void MainWindow::timerDown()
 
 MainWindow::~MainWindow()
 {
+    serial.close();
     delete ui;
 }
 
-
-void MainWindow::on_servoComboBox_currentIndexChanged(int index)
-{
-    servoControlMethod = index;
-    QString str = "svm " + QString::number(index);
-    serialWrite(str);
-}
-
 void MainWindow::serialWrite(QString &str) {
-    QByteArray writeData = str.toUtf8();
+    QByteArray writeData = str.toLatin1();
     qint64 bytesWritten = serial.write(writeData);
     if (bytesWritten == -1) {
         qDebug() << "Failed to write serial data" << endl;
@@ -99,41 +88,28 @@ void MainWindow::serialWrite(QString &str) {
 
 void MainWindow::on_servoPushButton_clicked()
 {
-    if (servoControlMethod == 0) {
-        QString str = "svc " + ui->servoAngleLineEdit->text() + " " + ui->servoSpeedLineEdit->text();
-        serialWrite(str);
-    }
-}
-
-void MainWindow::on_dcComboBox_currentIndexChanged(int index)
-{
-    dcControlMethod = index;
-    QString str = "dcm " + QString::number(index);
+    qDebug() << "Wrote servo position" << endl;
+    QString str = "sv " + ui->servoAngleLineEdit->text() + "\n";
     serialWrite(str);
 }
 
-void MainWindow::on_dcPushButton_clicked()
+void MainWindow::on_dcAnglePushButton_clicked()
 {
-    if (dcControlMethod == 0) {
-        QString str = "dcc " + ui->dcAngleLineEdit->text();
-        serialWrite(str);
-    } else if (dcControlMethod == 1) {
-        QString str = "dcc " + ui->dcVelocityLineEdit->text();
-        serialWrite(str);
-    }
+    qDebug() << "Wrote DC motor position" << endl;
+    QString str = "da " + ui->dcAngleLineEdit->text();
+    serialWrite(str);
 }
 
-void MainWindow::on_stepperComboBox_currentIndexChanged(int index)
+void MainWindow::on_dcVelocityPushButton_clicked()
 {
-    stepperControlMethod = index;
-    QString str = "stm " + QString::number(index);
+    qDebug() << "Wrote DC motor velocity" << endl;
+    QString str = "dv " + ui->dcVelocityLineEdit->text();
     serialWrite(str);
 }
 
 void MainWindow::on_stepperPushButton_clicked()
 {
-    if (stepperControlMethod == 0) {
-        QString str = "stc " + ui->stepperDisplacementLineEdit->text();
-        serialWrite(str);
-    }
+    qDebug() << "Wrote stepper displacement" << endl;
+    QString str = "st " + ui->stepperDisplacementLineEdit->text();
+    serialWrite(str);
 }
